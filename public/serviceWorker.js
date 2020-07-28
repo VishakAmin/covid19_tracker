254b37517953b78b5a1a3c5aabcf699baa43832f
VishakAmin <aminvishak@gmail.com>
2020-07-29 03:16:52 +0530

    Added PWA

---
+++ public/serviceWorker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "version-1"
+const urlsToCache = ["index.html", "offline.html"]
+
+const self = this;
+
+//install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Open Cache');
+                return cache.addAll(urlsToCache);
+
+            })
+
+    )
+});
+
+
+
+
+//listen
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html')
+                    )
+            })
+    )
+
+});
+
+//activate
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheNames) => {
+                if (!cacheWhiteList.includes(cacheNames)) {
+                    return caches.delete(cacheNames);
+                }
+            })
+        ))
+    )
+
+});
